6532a720ad4fccfe65d65a958746b72df13651a8
prachi913 <prachitiwari0911@gmail.com>
2023-03-20 23:24:43 +0530

    kk

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import styled from "styled-components"
+import { About } from './About';
+import { Home } from './Home';
 
 export const Navbar = () => {
     return (
-        <DIV>
-            <div className='div1'>me</div>
-            <div className='div2'>
-              
-                <Link to={"#"}>Home</Link>
-                <Link to={"#"}>About</Link>
-                <Link to={"#"}>Skill</Link>
-                <Link to={"#"}>Projects</Link>
-                <Link to={"#"}>Contact-Us</Link>
-            </div>
-        </DIV>
+      <DIV id=".nav-link.home">
+        <div className="div1">me</div>
+        <div className="div2">
+          <div id="#nav-menu">
+            <Home />
+          </div>
+          <div id="#nav-menu">
+            <About />
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </DIV>
     );
 };
 const DIV = styled.div`
